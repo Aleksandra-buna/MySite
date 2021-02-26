d455ab6b7db6a6df8932ce55ea25be24410c0aea
Aleksandra-buna <borabora0604@gmail.com>
2021-02-26 21:55:55 +0300

    Create apps 'blog' and 'events'

--- myawesomeblog/blog/models.py
+++ myawesomeblog/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    date = models.DateTimeField()
+    text = models.TextField(max_length=50000)
+    image = models.ImageField(upload_to='event_images/')
 # Create your models here.
